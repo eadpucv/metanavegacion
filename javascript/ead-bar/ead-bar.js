b60724fe6c4beb986a5c45405da897e64bcae142
Rodrigo Moya <rodrigo@citisent.com>
2014-05-05 16:31:51 -0300

    Imagenes en Amazon, Jquery y plugin base

---
+++ javascript/ead-bar/ead-bar.js
@@ -0,0 +1,11 @@
+(function ( $ ) {
+	$.fn.load_ead_bar = function(options) {
+
+
+		// Preparo el modal
+		// $(".ead_bar_modal_link").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+
+		return this;
+	};
+	
+}( jQuery ));
