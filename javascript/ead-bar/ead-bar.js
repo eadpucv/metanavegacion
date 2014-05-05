78329611cdfb5728a65d93c7b58eb1bf76541e3b
Rodrigo Moya <rodrigo@citisent.com>
2014-05-05 18:07:26 -0300

    Commit Incompleto - DropDown

--- javascript/ead-bar/ead-bar.js
+++ javascript/ead-bar/ead-bar.js
@@ -1,9 +1,25 @@
 (function ( $ ) {
 	$.fn.load_ead_bar = function(options) {
+		
+		// Cargo Modales (desde bucked e)
+		$.getScript("include/jquery.leanModal.min.js", function(){
+			alert(";-)")
+			//$(".ead_bar_modal_link").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+		});
 
+		// DropDowns
+		$('li.ead-bar-dropdown ul').hide().removeClass('fallback');
+		$('nav li.ead-bar-dropdown').hover(
+		  function () {
+		    $('ul', this).stop().slideDown(100);
+		  },
+		  function () {
+		    $('ul', this).stop().slideUp(100);
+		  }
+		);
 
-		// Preparo el modal
-		// $(".ead_bar_modal_link").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+		// Buscador
+
 
 		return this;
 	};
