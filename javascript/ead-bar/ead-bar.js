6cbd15c6886dc4f1b3c17c2adb23f268d0e8afb9
Rodrigo Moya <rodrigo@citisent.com>
2014-05-06 09:51:32 -0300

    Modal

--- javascript/ead-bar/ead-bar.js
+++ javascript/ead-bar/ead-bar.js
@@ -2,24 +2,22 @@
 	$.fn.load_ead_bar = function(options) {
 		
 		// Cargo Modales (desde bucked e)
-		$.getScript("include/jquery.leanModal.min.js", function(){
-			alert(";-)")
-			//$(".ead_bar_modal_link").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
+		$.getScript("https://s3.amazonaws.com/Ead/ead_bar_js/jquery.leanModal.min.js", function(){
+			$(".ead_bar_modal_link").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
 		});
 
 		// DropDowns
 		$('li.ead-bar-dropdown ul').hide();
 		$('nav li.ead-bar-dropdown').hover(
-		  function () {
-		    $('ul', this).stop().slideDown(100);
-		  },
-		  function () {
-		    $('ul', this).stop().slideUp(100);
-		  }
+		  	function () {
+				$('ul', this).stop().slideDown(100);
+		  	},
+		  	function () {
+				$('ul', this).stop().slideUp(100);
+		  	}
 		);
 
 		// Buscador
-
 
 		return this;
 	};
