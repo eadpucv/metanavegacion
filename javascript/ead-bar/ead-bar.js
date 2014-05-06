1166fd719a5f3c1d943be1429a7e25d446ebc74d
Rodrigo Moya <rodrigo@citisent.com>
2014-05-05 21:42:58 -0300

    Correcciones

--- javascript/ead-bar/ead-bar.js
+++ javascript/ead-bar/ead-bar.js
@@ -8,7 +8,7 @@
 		});
 
 		// DropDowns
-		$('li.ead-bar-dropdown ul').hide().removeClass('fallback');
+		$('li.ead-bar-dropdown ul').hide();
 		$('nav li.ead-bar-dropdown').hover(
 		  function () {
 		    $('ul', this).stop().slideDown(100);
