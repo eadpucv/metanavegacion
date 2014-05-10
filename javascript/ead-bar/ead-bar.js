b3f54bd1783bdea3a44702d8bbce7c8a3a2c99e1
Rodrigo Moya <rodrigo@citisent.com>
2014-05-10 16:07:32 -0300

    movimiento de javascript

--- javascript/ead-bar/ead-bar.js
+++ javascript/ead-bar/ead-bar.js
@@ -1,21 +1,9 @@
 (function ( $ ) {
 	$.fn.load_ead_bar = function(options) {
 		
-		// Librerias Auxiliares
-		$.getScript("https://s3.amazonaws.com/Ead/ead_bar_js/jquery.leanModal.min.js", function(){
-			$(".ead_bar_modal_link").leanModal({ top : 200, overlay : 0.4, closeButton: ".modal_close" });
-		});
-
-		// Activacion de los dropDowns
-		$('li.ead-bar-dropdown ul').hide();
-		$('nav li.ead-bar-dropdown').hover(
-		  	function () {
-				$('ul', this).stop().slideDown(100);
-		  	},
-		  	function () {
-				$('ul', this).stop().slideUp(100);
-		  	}
-		);
+		// Cargar html
+		this.prepend("<div id='bar'></div>");
+		$('#bar').load(options.path_js+'bar.html');
 
 		// Identificar Sitio
 
