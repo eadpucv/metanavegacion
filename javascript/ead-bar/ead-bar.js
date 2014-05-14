5246176969d902d6748aa9e6d83ac664204c2852
Rodrigo Moya <rodrigo@citisent.com>
2014-05-13 21:42:28 -0300

    Menu para moviles

--- javascript/ead-bar/ead-bar.js
+++ javascript/ead-bar/ead-bar.js
@@ -34,12 +34,24 @@
 
 				// Asigno Textos y Url's
 				$("#ead-bar-dropdown_li").prepend("<a href='#' id='ead_bar_dropdown-session' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>"+ options.name + " " + options.lastname + "</a>");
-				$("#ead_bar_dropdown-perfil").attr('href','http://www.com.com');
+				$("#ead_bar_dropdown-perfil").attr('href','http://wiki');
 				$("#ead_bar_dropdown-account").attr('href',"http://personas.ead.pucv.cl/usuarios/edit/"+options.id);
-				$("#ead_bar_dropdown-end_session").attr('href',options.site_logout_url);								
+				$("#ead_bar_dropdown-end_session").attr('href',options.site_logout_url);
+
+				$("#ead-bar-dropdown_li-movil").prepend("<a href='#' id='ead_bar_dropdown-session-movil' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>"+ options.name + " " + options.lastname + "</a>");
+				$("#ead_bar_dropdown-perfil-movil").attr('href','http://wiki');
+				$("#ead_bar_dropdown-account-movil").attr('href',"http://personas.ead.pucv.cl/usuarios/edit/"+options.id);
+				$("#ead_bar_dropdown-end_session-movil").attr('href',options.site_logout_url);
+
+				$("#ead_bar_dropdown-session-movil").click(function() {
+					$("#ead_bar_user_menu-movil").toggle();
+				});
+				
+
 			} else {
 				// Habilitar boton y link para iniciar session
 				$("#ead-bar-dropdown_li").prepend("<a href='"+options.site_login_url+"' id='ead_bar_dropdown-session' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>Iniciar sesión</a>")
+				$("#ead-bar-dropdown_li-movil").prepend("<a href='"+options.site_login_url+"' id='ead_bar_dropdown-session' class='link iniciarsesion'><img class='icono' src='https://s3.amazonaws.com/Ead/ead_bar_images/icon-usuario.gif'>Iniciar sesión</a>")				
 			}
 
 			// Identificar Sitio
